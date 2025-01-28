eb263a76ef9f8c420a5af30efa43326c7f494d24
Youssef Aboukir <yaboukir@e2r4p5.1337.ma>
2025-01-28 21:04:42 +0100

    push_swap

---
+++ mandatory/ft_atoi.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaboukir <yaboukir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/24 21:18:25 by yaboukir          #+#    #+#             */
+/*   Updated: 2025/01/28 20:46:47 by yaboukir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static	int	ft_whitespaces(int c)
+{
+	if (c == ' ' || (c >= 9 && c <= 13))
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	size_t				i;
+	long				sign;
+	unsigned long long	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (ft_whitespaces(str[i]))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i++] == '-')
+			sign *= -1;
+	}
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + (str[i++] - '0');
+		if ((sign && result > INT_MAX)
+			|| (sign < 1 && result > ((long)INT_MAX + 1)))
+		{
+			ft_putstr("Error\n");
+			exit(1);
+		}
+	}
+	return (sign * result);
+}
---
+++ mandatory/struct_utils2.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   struct_utils2.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaboukir <yaboukir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/24 23:34:50 by yaboukir          #+#    #+#             */
+/*   Updated: 2025/01/25 18:34:39 by yaboukir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	*last_node(t_stack *a)
+{
+	if (!a)
+		return (NULL);
+	while (a->next)
+	{
+		a = a->next;
+	}
+	return (a);
+}
+
+void	*pre_last_node(t_stack *a)
+{
+	if (!a)
+		return (NULL);
+	while (a->next && a->next->next)
+		a = a->next;
+	return (a);
+}
+
+void	ft_lstclear(t_stack **stack)
+{
+	t_stack	*tmp;
+	t_stack	*node;
+
+	if (!stack)
+		return ;
+	node = *stack;
+	while (node != NULL)
+	{
+		tmp = node->next;
+		free(node);
+		node = tmp;
+	}
+	*stack = NULL;
+}
+
+int	ft_sorted(t_stack	**a)
+{
+	t_stack	*tmp;
+
+	tmp = *a;
+	while (tmp->next)
+	{
+		if (tmp->value > tmp->next->value)
+			return (1);
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
+int	ft_strlen2(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
